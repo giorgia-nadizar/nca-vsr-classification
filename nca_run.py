c69b6d57eddd0bb2ef6ada28c26ba4cda3f5a402
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2022-11-28 21:26:21 +0100

    New run parameter for pretty printing the result.

--- nca_run.py
+++ nca_run.py
@@ -9,14 +9,21 @@ from nca import Node
 from nca_training import load_shapes_from_file, parse_shape
 
 
-def print_vals(vals, width: int, height: int, n_classes: int) -> None:
-  for i in range(height):
-    print("".join(
-      [str(np.argmax(np.frombuffer(vals[i][j], dtype=np.float32)[-n_classes:])) if vals[i][j] is not None else '-'
-       for j in range(width)]))
+def print_vals(vals, width: int, height: int, n_classes: int, pretty_print: bool = True) -> None:
+  if pretty_print:
+    for i in range(height):
+      print("".join(
+        [f'({np.argmax(np.frombuffer(vals[i][j], dtype=np.float32)[-n_classes:]):02d})'
+         if vals[i][j] is not None else '    ' for j in range(width)]))
+  else:
+    for i in range(height):
+      for j in range(width):
+        if vals[i][j] is not None:
+          print(f'{j},{i},{np.argmax(np.frombuffer(vals[i][j], dtype=np.float32)[-n_classes:])}')
 
 
-def main(sleep: bool, display_transient: bool, target_set: int, target_shape: str, n_steps: int, deterministic: bool):
+def main(sleep: bool, display_transient: bool, target_set: int, target_shape: str, n_steps: int, deterministic: bool,
+         pretty_print: bool):
   shapes = load_shapes_from_file('shapes/sample_creatures_set' + str(target_set) + '.txt')
   x = shapes[int(target_shape)] if target_shape.isnumeric() else parse_shape(target_shape)
 
@@ -47,7 +54,7 @@ def main(sleep: bool, display_transient: bool, target_set: int, target_shape: st
     else:
       Node.stochastic_update(nodes)
     if display_transient or n == n_steps - 1:
-      print_vals(vals, width, height, len(shapes))
+      print_vals(vals, width, height, len(shapes), pretty_print)
     if display_transient:
       print('')
     if sleep:
@@ -57,10 +64,11 @@ def main(sleep: bool, display_transient: bool, target_set: int, target_shape: st
 if __name__ == '__main__':
   m_sleep = False
   m_display_transient = False
-  m_target_set = 1
-  m_target_shape = '0'
+  m_target_set = 3
+  m_target_shape = '11'
   m_n_steps = 50
   m_deterministic = True
+  m_pretty_print = True
 
   args = sys.argv[1:]
   for arg in args:
@@ -76,5 +84,7 @@ if __name__ == '__main__':
       m_display_transient = arg.replace('display_transient=', '').lower().startswith('t')
     elif arg.startswith('deterministic'):
       m_deterministic = arg.replace('deterministic=', '').lower().startswith('t')
+    elif arg.startswith('pretty_print'):
+      m_pretty_print = arg.replace('pretty_print=', '').lower().startswith('t')
 
-  main(m_sleep, m_display_transient, m_target_set, m_target_shape, m_n_steps, m_deterministic)
+  main(m_sleep, m_display_transient, m_target_set, m_target_shape, m_n_steps, m_deterministic, m_pretty_print)
