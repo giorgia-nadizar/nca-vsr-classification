87aae0fc7811bd9618cdf77c3c06c440e86a4399
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2022-11-28 10:00:51 +0100

    Main method created.

--- nca_run.py
+++ nca_run.py
@@ -10,33 +10,14 @@ from nca import Node
 from nca_training import load_shapes_from_file, parse_shape
 
 
-def print_vals(width: int, height: int, n_classes: int) -> None:
+def print_vals(vals, width: int, height: int, n_classes: int) -> None:
   for i in range(height):
     print("".join(
       [str(np.argmax(np.frombuffer(vals[i][j], dtype=np.float32)[-n_classes:])) if vals[i][j] is not None else '-'
        for j in range(width)]))
 
 
-if __name__ == '__main__':
-  sleep = False
-  display_transient = False
-  target_set = 1
-  target_shape = '0'
-  n_steps = 50
-
-  args = sys.argv[1:]
-  for arg in args:
-    if arg.startswith('set'):
-      target_set = int(arg.replace('set=', ''))
-    elif arg.startswith('shape'):
-      target_shape = arg.replace('shape=', '')
-    elif arg.startswith('steps'):
-      n_steps = int(arg.replace('steps=', ''))
-    elif arg.startswith('sleep'):
-      sleep = arg.replace('sleep=', '').lower().startswith('t')
-    elif arg.startswith('display_transient'):
-      display_transient = arg.replace('display_transient=', '').lower().startswith('t')
-
+def main(sleep: bool, display_transient: bool, target_set: int, target_shape: str, n_steps: int):
   shapes = load_shapes_from_file('shapes/sample_creatures_set' + str(target_set) + '.txt')
   x = shapes[int(target_shape)] if target_shape.isnumeric() else parse_shape(target_shape)
 
@@ -67,8 +48,31 @@ if __name__ == '__main__':
       node = nodes[num]
       node.forward()
     if display_transient or n == n_steps - 1:
-      print_vals(width, height, len(shapes))
+      print_vals(vals, width, height, len(shapes))
     if display_transient:
       print('')
     if sleep:
       time.sleep(0.1)
+
+
+if __name__ == '__main__':
+  m_sleep = False
+  m_display_transient = False
+  m_target_set = 1
+  m_target_shape = '0'
+  m_n_steps = 50
+
+  args = sys.argv[1:]
+  for arg in args:
+    if arg.startswith('set'):
+      m_target_set = int(arg.replace('set=', ''))
+    elif arg.startswith('shape'):
+      m_target_shape = arg.replace('shape=', '')
+    elif arg.startswith('steps'):
+      m_n_steps = int(arg.replace('steps=', ''))
+    elif arg.startswith('sleep'):
+      m_sleep = arg.replace('sleep=', '').lower().startswith('t')
+    elif arg.startswith('display_transient'):
+      m_display_transient = arg.replace('display_transient=', '').lower().startswith('t')
+
+  main(m_sleep, m_display_transient, m_target_set, m_target_shape, m_n_steps)
