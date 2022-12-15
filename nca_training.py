4d74f231c52f06f8ac0ab3a98aa1e64b441c35d8
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2022-12-15 22:04:31 +0100

    Classification analysis added.

--- nca_training.py
+++ nca_training.py
@@ -62,7 +62,7 @@ def train(x_train: list[list[list]], num_iterations: int = 1500, plots: bool = F
 
   trainer = tf.keras.optimizers.Adam()
   losses = []
-  accs = []
+  accuracies = []
 
   x0 = tf.constant(x_train)  # (bs, y, x)
   mask = tf.expand_dims(x0, axis=3)  # (bs, y, x, 1)
@@ -88,8 +88,8 @@ def train(x_train: list[list[list]], num_iterations: int = 1500, plots: bool = F
     y_label = tf.argmax(y0, axis=-1)  # (bs, y, x)
     x_label = tf.argmax(x, axis=-1)  # (bs, y, x)
     correct = tf.cast(tf.equal(x_label, y_label), tf.float32)  # (bs, y, x)
-    acc = tf.reduce_sum(correct * x0) / tf.reduce_sum(x0)
-    accs.append(acc.numpy().item())
+    accuracy = tf.reduce_sum(correct * x0) / tf.reduce_sum(x0)
+    accuracies.append(accuracy.numpy().item())
 
   if plots:
     pl.figure(figsize=(10, 4))
@@ -101,25 +101,25 @@ def train(x_train: list[list[list]], num_iterations: int = 1500, plots: bool = F
     pl.show()
 
     pl.figure(figsize=(10, 4))
-    pl.title('accs')
+    pl.title('Accuracies')
     pl.xlabel('Number of steps')
-    pl.ylabel('accs')
-    pl.plot(accs, label="ca")
+    pl.ylabel('Accuracies')
+    pl.plot(accuracies, label="ca")
     pl.legend()
     pl.show()
 
-  return model, losses, accs
+  return model, losses, accuracies
 
 
 def train_and_pickle(set_number: int, num_iterations: int = 1500, save_progress=True):
   shapes = load_shapes_from_file('shapes/sample_creatures_set' + str(set_number) + '.txt')
-  model, losses, accs = train(shapes, num_iterations, plots=False)
+  model, losses, accuracies = train(shapes, num_iterations, plots=False)
 
   if save_progress:
     with open(f'training/progress_{set_number}.txt', 'w') as f:
-      f.write('iteration;loss;acc\n')
+      f.write('iteration;loss;accuracy\n')
       for iteration in range(num_iterations):
-        f.write(f'{iteration};{losses[iteration].numpy()};{accs[iteration]}\n')
+        f.write(f'{iteration};{losses[iteration].numpy()};{accuracies[iteration]}\n')
 
   perceive_kernel, pb = model.perceive.layers[0].get_weights()
   dk1, db1 = model.dmodel.layers[0].get_weights()
