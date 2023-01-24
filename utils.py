a9e0e86be5b54263a216e345831ca9890a313d2b
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2023-01-24 14:45:45 +0100

    Pgfplots utils added.

--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@ import bz2
 import _pickle
 import re
 
+import numpy as np
+from matplotlib import pyplot as plt
+from numpy import median
+from pandas import DataFrame
+
 
 class PicklePersist:
 
@@ -52,3 +57,60 @@ class ShapeUtils:
       if shape:
         shapes.append(shape)
     return shapes
+
+
+class PgfplotsUtils:
+
+  @staticmethod
+  def line_plot(df: DataFrame, x: str, y: list, groups: list[str], filename: str):
+    def q1(x):
+      return x.quantile(0.25)
+
+    def q3(x):
+      return x.quantile(0.75)
+
+    vals = dict([(key, [q1, q3, median]) for key in y])
+
+    summary = df.groupby(groups + [x]).agg(vals)
+    summary.columns = ["_".join(col) for col in summary.columns.to_flat_index()]
+    summary.reset_index(inplace=True)
+
+    key_df = df.drop_duplicates(subset=groups)
+
+    for i in range(len(key_df)):
+      tmp = summary
+      current_filename = filename
+      for key in groups:
+        tmp = tmp[tmp[key] == key_df[key].iloc[i]]
+        current_filename += f"_{key_df[key].iloc[i]}"
+      tmp.to_csv(f"{current_filename}.txt", sep="\t", index=False)
+
+  @staticmethod
+  def box_plot(df: DataFrame, x: str, y: str, groups: list[str], filename: str):
+    key_df = df.drop_duplicates(subset=groups)
+    plt.figure(visible=False)
+
+    for i in range(len(key_df)):
+      tmp = df
+      current_filename = filename
+      for key in groups:
+        tmp = tmp[tmp[key] == key_df[key].iloc[i]]
+        current_filename += f"_{key_df[key].iloc[i]}"
+
+      data = []
+      for xi in tmp[x].unique():
+        data.append([k for k in tmp[tmp[x] == xi][y] if str(k) != "nan"])
+
+      bp = plt.boxplot(data, showmeans=False)
+
+      minimums = [round(item.get_ydata()[0], 1) for item in bp['caps']][::2]
+      q1 = [round(min(item.get_ydata()), 1) for item in bp['boxes']]
+      medians = [item.get_ydata()[0] for item in bp['medians']]
+      q3 = [round(max(item.get_ydata()), 1) for item in bp['boxes']]
+      maximums = [round(item.get_ydata()[0], 1) for item in bp['caps']][1::2]
+
+      rows = [tmp[x].unique().tolist(), minimums, q1, medians, q3, maximums]
+
+      with open(f"{current_filename}.txt", "w") as bp_file:
+        for row in rows:
+          bp_file.write("\t".join(map(str, row)) + "\n")
